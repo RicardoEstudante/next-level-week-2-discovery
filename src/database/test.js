6cc020a03f300e2034084439e0cfe0474afa0b8f
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2021-01-12 10:34:03 -0300

    115

--- src/database/test.js
+++ src/database/test.js
@@ -2,7 +2,7 @@ const Database = require('./db');
 const createProffy = require('./createProffy');
 
 
-Database.then((db) => {
+Database.then(async (db) => {
     proffyValue = {
             name: "Ricardo Carvalho",
             avatar: "https://avatars0.githubusercontent.com/u/42756551?s=60&v=4",
@@ -10,12 +10,12 @@ Database.then((db) => {
             bio: "Entusiasta das melhores tecnologias de química avançada.",
         }
 
-        classValue = {
+    classValue = {
             subject: "História",
             cost: "20",
         }
         
-        classScheduleValue = [{
+    classScheduleValues = [{
             weekday: 1,
             time_from: 720,
             time_to: 1220,
@@ -27,5 +27,5 @@ Database.then((db) => {
         }
     ]
 
-    // createProffy(db, {proffyValue, classValue, classScheduleValue});
+    await createProffy(db, {proffyValue, classValue, classScheduleValues});
 })
