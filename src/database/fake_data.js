d4d1da807b5951a893e85632da07035cdafc15db
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2021-01-14 20:04:38 -0300

    155min

---
+++ src/database/fake_data.js
@@ -0,0 +1,24 @@
+const proffys = [
+    {
+        name: "Ricardo Carvalho",
+        avatar: "https://avatars0.githubusercontent.com/u/42756551?s=60&v=4",
+        whatsapp: "11999999999",
+        bio: "Entusiasta das melhores tecnologias de química avançada.",
+        subject: "História",
+        cost: "20",
+        weekday: [0],
+        time_from: [720],
+        time_to: [1220],
+    },
+    {
+        name: "Teste",
+        avatar: "https://avatars0.githubusercontent.com/u/42756551?s=60&v=4",
+        whatsapp: "11999999999",
+        bio: "Entusiasta das melhores tecnologias de química avançada.",
+        subject: "História",
+        cost: "20",
+        weekday: [1],
+        time_from: [720],
+        time_to: [1220],
+    }
+];
