e97be0d36afef3d4dabff8fa02234d0706878ae1
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2021-01-15 11:47:45 -0300

    FInish

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const server = express();
 
-const { pageLanding, pageStudy, pageGiveClasses } = require('./pages')
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require('./pages')
 
 const nunjucks = require('nunjucks');
 
@@ -10,11 +10,14 @@ nunjucks.configure('src/views', {
     noCache: true,
 });
 
+server.use(express.urlencoded({ extended: true }));
+
 server.use(express.static("public"));
 
 server.get('/', pageLanding);
 server.get('/study', pageStudy);
 server.get('/give-classes', pageGiveClasses);
+server.post('/save-classes', saveClasses);
 
 server.listen(3333, () => { 
     console.log("Server onFire🔥");
